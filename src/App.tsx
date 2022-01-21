038c3aac3268425c0bbd9d723eb937122c5c179a
Xavier Rutayisire <xrutayisire@wallix.com>
2022-01-21 12:44:06 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Cron from "react-js-cron";
+import { useTranslation } from "react-i18next";
+import "antd/dist/antd.css";
 
 function App() {
+  const { t } = useTranslation();
+  const [value, setValue] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {t("Something")}
+      <Cron value={value} setValue={setValue} />
     </div>
   );
 }
